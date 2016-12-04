f1faf17acabeb7a3647f6275e943d36ff332d69b
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2016-12-04 13:00:49 +0000

    Add tests for blank and non-hex string.

--- src/test/scala/GameTrackerSpec.scala
+++ src/test/scala/GameTrackerSpec.scala
@@ -1,6 +1,6 @@
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers, OneInstancePerTest}
 
-class GameTrackerSpec extends FlatSpec with Matchers with TestDataUtils {
+class GameTrackerSpec extends FlatSpec with Matchers with OneInstancePerTest with TestDataUtils {
 
   //  Write a utility that:
   //  • can parse incoming data items into a data structure suitable for capturing the state of the match
@@ -13,13 +13,13 @@ class GameTrackerSpec extends FlatSpec with Matchers with TestDataUtils {
   //  • handles cases where an invalid data item is received or where data is inconsistent with previous
   //  data received.
 
+  val gameTracker = GameTracker()
+
   "The game tracker" should "start from a game that has not started yet" in {
-    GameTracker().currentState should be(GameJustStartedEvent)
+    gameTracker.currentState should be(GameJustStartedEvent)
   }
 
   it should "receive input and return it when queried" in {
-    val gameTracker = GameTracker()
-
     gameTracker.updateStateFrom(specExampleTeam1ScoresHex)
     gameTracker.currentState should be(specExampleTeam1ScoresEvent)
 
@@ -28,7 +28,6 @@ class GameTrackerSpec extends FlatSpec with Matchers with TestDataUtils {
   }
 
   it should "return the last n events that happened" in {
-    val gameTracker = GameTracker()
     gameTracker.updateStateFrom(specExampleTeam1ScoresHex)
     gameTracker.updateStateFrom(specExampleTeam2ScoresBackHex)
 
@@ -36,18 +35,32 @@ class GameTrackerSpec extends FlatSpec with Matchers with TestDataUtils {
   }
 
   it should "return all events if n requested but less than n occurred" in {
-    val gameTracker = GameTracker()
     gameTracker.updateStateFrom(specExampleTeam1ScoresHex)
 
     gameTracker.getLastNEvents(5) should be(List(GameJustStartedEvent, specExampleTeam1ScoresEvent))
   }
 
   it should "return all events when queried" in {
-    val gameTracker = GameTracker()
     gameTracker.updateStateFrom(specExampleTeam1ScoresHex)
     gameTracker.updateStateFrom(specExampleTeam2ScoresBackHex)
 
     gameTracker.getAllEvents should be(List(GameJustStartedEvent, specExampleTeam1ScoresEvent, specExampleTeam2ScoresBackEvent))
+  }
+
+  it should "not update the state of the match if the input is incorrect and causes an exception" in {
+    gameTracker.updateStateFrom("abcde")
+
+    val state = gameTracker.currentState
+
+    state should be(GameJustStartedEvent)
+  }
+
+  it should "not update the state of the match if the input is a blank string" in {
+    gameTracker.updateStateFrom("")
+
+    val state = gameTracker.currentState
+
+    state should be(GameJustStartedEvent)
   }
 
 }
